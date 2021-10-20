fa1f08e3c1b4bba1f843c556d7a09ec0aecae6be
Diego Alvarez S <dialvarezs@gmail.com>
2021-10-20 20:12:28 -0300

    Update container generation script

--- scripts/container_generator.jl
+++ scripts/container_generator.jl
@@ -5,23 +5,23 @@ mkpath("container")
 cd("container")
 
 pkgs = [
-    (name = "ArgMacros", version = "0.2.3")
+    (name = "ArgMacros", version = "0.2.4")
     (name = "ArgParse", version = "1.1.4")
     (name = "BioAlignments", version = "2.0.0")
     (name = "BioSequences", version = "2.0.5")
     (name = "CodecZlib", version = "0.7.0")
     (name = "CSV", version = "0.8.5")
-    (name = "DataStructures", version = "0.18.9")
+    (name = "DataStructures", version = "0.18.10")
     (name = "Edlib", version = "0.1.1")
-    (name = "FASTX", version = "1.1.3")
-    (name = "JSON3", version = "1.8.2")
+    (name = "FASTX", version = "1.2.0")
+    (name = "JSON3", version = "1.9.2")
     (name = "Query", version = "1.0.0")
-    (name = "StructTypes", version = "1.7.2")
+    (name = "StructTypes", version = "1.8.1")
     (name = "XAM", version = "0.2.7")
 ]
 no_test = [x.name for x in pkgs]
 
-julia_version = v"1.6.1"
+julia_version = v"1.6.3"
 parent_image = "ubuntu:20.04"
 apt_override = String[
     # absolutely necessary
@@ -47,16 +47,3 @@ SimpleContainerGenerator.create_dockerfile(
     parent_image = parent_image,
     override_default_apt = apt_override,
 )
-
-
-# build image using Podman, "transform" to Singularity and upload to library
-local_image_tag = "dialvarezs/10x_consensus"
-library_image_url = "dialvarezs/default/10x-consensus:latest"
-basename = split(local_image_tag, "/")[2]
-
-run(`podman build -t $(local_image_tag) .`)
-run(`podman save --format oci-archive --output $(basename).tar $(local_image_tag)`)
-run(`singularity build -F $(basename).sif oci-archive://$(basename).tar`)
-run(`rm -fr $(basename).tar Dockerfile simplecontainergenerator_container_files`)
-run(`singularity sign $(basename).sif`)
-run(`singularity push $(basename).sif library://$(library_image_url)`)
